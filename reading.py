aed297145f43a256ff08a87d6a3ead7df7fdfe4d
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-02-09 18:01:52 +0000

    Add a funtion to prevent loop execution

--- reading.py
+++ reading.py
@@ -8,3 +8,10 @@ def read_file_executed():
     file.close() #close the file
 
     return content
+
+def remove_func_from_code(content, function_name): #remove the regex pattern from the string
+    pattern = re.compile(rf'(.)*{re.escape(function_name)}\(\)') #create a regex for *.function_name()
+    lines = content.split('\n')
+    new_lines = [line for line in lines if not pattern.search(line)]
+    exec_string = '\n'.join(new_lines).strip()
+    return exec_string
