4ecb0416213d70c247220cf09438bfc601031d45
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-02-12 17:43:32 +0000

    Excluding useless files

--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def teste():
-    with open(sys.argv[0], mode='r') as file:
-        content = file.read()
-        file.close()
-    return content # Imprime o caminho do arquivo atual
