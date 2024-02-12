5c7727ddd3b7edc1d3fdeebb1a71f1a3cabb87d2
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-02-12 15:16:39 +0000

    Corrected the Open AI API

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -1,23 +1,28 @@
 
 import openai
+import sys
+import os
 import re
 from samples_and_prompt import prompt_template, sample_response
 
-# openai.api_key = "SUA_API_KEY"
+OPENAI_API = "YOUR_API_KEY"
+
+client = openai.OpenAI(api_key= OPENAI_API)
 
 def generate_response(code, error):
-    code = code
-    e = error
-    prompt = prompt_template
-    if openai.api_key is not None:
-        response = openai.Completion.create(
-        engine="davinci-codex",  
-        prompt=prompt,
-        max_tokens=150,  
-        temperature=0.2,
-        stop=None).choices[0].text.split()
-    else:
-        response = sample_response
+    prompt = prompt_from_template(code, error)
+    try:
+        response = client.chat.completions.create(
+        model="gpt-3.5-turbo",  
+        messages=[{"role": "system", "content": prompt}],
+        max_tokens=2000,  
+        temperature=0.2).choices[0].message.content
+    except:
+        if sys.argv[0] == f'{os.getcwd()}/sample_file.py':
+            response = sample_response
+        else:
+            print('You must defined your API KEY')
+            sys.exit(0)
 
     return response
 
@@ -25,3 +30,11 @@ def extract_python_code(text):
     pattern = r'```python(.*?)```'
     python_code = re.search(pattern, text, re.DOTALL).group(1).strip()
     return python_code
+
+def prompt_from_template(code, error):
+     return f'''{prompt_template[0]} {code}
+
+Traceback: {error}
+
+{prompt_template[1]}
+'''
--- samples_and_prompt/prompt.py
+++ samples_and_prompt/prompt.py
@@ -2,15 +2,10 @@
 
 code, e = None, None
 
-prompt_template = f'''
+prompt_template = {0: '''
 I am coding. It is a Python script, and it is not running. It has one or multiple errors. I will give you the code and the traceback. You should correct all the errors that you find, not just the traceback. Correct the errors in the provided Python script without looking at the traceback first, then look at the traceback and see if you have already corrected it. 
 
-Code: 
-'{code}'
-
-Traceback: '{e}'
-
-You should begin your answer with the code; there should be no other word before it.
+Code:''', 1:'''You should begin your answer with the code; there should be no other word before it.
 
 After the code, you should explain, with bullet points the correction you made, in as few words as possible, limited to 200 words.
 
@@ -21,4 +16,4 @@ print('hello word')
 
 - fixed print
 -added parentheses
-'''
+'''}
